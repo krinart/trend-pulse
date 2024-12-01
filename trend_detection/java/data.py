fcba830ffebf602584c602443a7885594cc576c4
Viktor Yershov <krinart@gmail.com>
2024-12-01 12:55:36 -0800

    Update Trend Generation

--- trend_detection/java/data.py
+++ trend_detection/java/data.py
@@ -1,8 +1,8 @@
 import json
 import pandas as pd
 
-data = json.load(open("data/trend_messages_v25.json"))
-with open("java/data/messages_rows_with_id.json", "a") as f:
+data = json.load(open("../data/trend_messages_v25.json"))
+with open("data/messages_rows_with_id_v26.json", "a") as f:
 	for row in data:
 		f.write(json.dumps(row) + "\n")
 
